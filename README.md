a1d741285bb4cbde4ba87d048ad6f07733f57759
Joshua Hunt <joshhunt.info@gmail.com>
2019-08-03 14:37:52 -0400

    Part 1 = Done

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 - Aug 3rd - Easy Saturday project
     - Calculator from https://zellwk.com (found off freecodecamp but let's support the smaller business)
+        - Code by Zell Liew
+
+----------------------------------------------------------------------------------------------------------
 
 - Finished the HTML/CSS, onwards to the JS!
+
+- Working on the JS. 
+    - button output to console.log() is working!
+
+- This is a good guide because it is also showing me the logic behind the choices of the user of the calculator
+    - User will always hit a number after an operator
+    - User will hit a number after a decimal
+    - Stuff like that
+
+- Done Part 1!
--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const calculate = (n1, operator, n2) => {
+    let result = ""
+    if (operator === "add") {
+        result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === "subtract") {
+        result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === "multiply") {
+        result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === "divide") {
+        result = parseFloat(n1) / parseFloat(n2)
+    }
+    return result
+}
+
 const calculator = document.querySelector(".calculator")
 const display = calculator.querySelector(".calculator__display")
 const keys = calculator.querySelector(".calculator__keys")
@@ -10,25 +24,31 @@ keys.addEventListener("click", e => {
         const keyContent = key.textContent
         const displayedNum = display.textContent
         const previousKeyType = calculator.dataset.previousKeyType
-
+        
         Array.from(key.parentNode.children)
         .forEach(k => k.classList.remove("is-depressed"))
 
         if (!action) {
-            console.log("number key!")
+            if (displayedNum === "0" || previousKeyType === "operator" ) {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
         }
-        
         if (
             action === "add" ||
             action === "subtract" ||
             action === "multiply" ||
             action === "divide"
         ) {
-            console.log("operator key!")
+            key.classList.add("is-depressed")
+            calculator.dataset.previousKeyType = "operator"
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
         }
         
         if (action === "decimal") {
-            console.log("decimal key!")
+            display.textContent = displayedNum + "."
         }
         
         if (action === "clear") {
@@ -36,9 +56,11 @@ keys.addEventListener("click", e => {
         }
         
         if (action === "calculate") {
-            console.log("equal key!")
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
 
-        display.textContent = calculate(firstValue, operator, secondValue)
+            display.textContent = calculate(firstValue, operator, secondValue)
         }
     }
 })
